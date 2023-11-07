bf2284ac07ad9c171d135fd0e20a6a463d491296
Aslam AKS <aslamaksofficial@gmail.com>
2023-11-07 18:22:01 +0530

    calculator 90%

--- src/Components/Calculator/Calculator.js
+++ src/Components/Calculator/Calculator.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import "./Calculator.css";
 import { IoIosMenu } from "react-icons/io";
 import { RxCross1, RxBox } from "react-icons/rx";
@@ -7,24 +6,29 @@ import { BsBackspace } from "react-icons/bs";
 import { PiDivideLight } from "react-icons/pi";
 import { GoDash, GoPlus } from "react-icons/go";
 import { TbSquareRoot2 } from "react-icons/tb";
-import {FcHome} from 'react-icons/fc'
 
 function Calculator() {
   const [current, setCurrent] = useState("");
   const [prevoius, setPrevoius] = useState("");
   const [operations, setOperations] = useState("");
+  const [seconNum, setSecondNum] = useState("");
+  const [equal, setEqual] = useState("");
+  const [math, setMath] = useState("");
 
-    const deleteHandler = (value) => {
-      if(value==='dl'){
-        setCurrent(String(current).slice(0, -1));
-      }
-    };
+  const deleteHandler = (value) => {
+    if (value === "dl") {
+      setCurrent(String(current).slice(0, -1));
+    }
+  };
 
   const allclearHandler = (value) => {
     if (value === "CE") {
       setCurrent("");
       setOperations("");
       setPrevoius("");
+      setEqual("");
+      setSecondNum("");
+      setMath("");
     }
   };
 
@@ -34,34 +38,93 @@ function Calculator() {
     }
   };
 
-  const calculateRoot = () => {
-    try {
-      setPrevoius(current)
-      let prv = current ? prevoius : current;
-      const result = Math.sqrt(parseFloat(current));
-      setCurrent(result);
-      setPrevoius("root(" + prv + ")");
-    } catch (error) {
-      setCurrent("Invalid Input");
-    }
-  };
-
   const calculatePercentage = () => {
     try {
       const result = (parseFloat(current) / 100).toString();
-      setCurrent(result);
+
+      if (!operations) {
+        setEqual("");
+        setOperations("");
+        setSecondNum("");
+        setPrevoius("0");
+        setCurrent("0");
+      } else if (equal) {
+        setEqual("");
+        setOperations("");
+        setSecondNum("");
+        setCurrent(result);
+        setPrevoius(result);
+      } else {
+        setCurrent(result);
+        setSecondNum(result);
+      }
     } catch (error) {
       setCurrent("ERROR");
     }
   };
 
+  const calculateRoot = () => {
+    try {
+      if (prevoius && operations) {
+        setSecondNum(math);
+        if (equal) {
+          setEqual("");
+          setOperations("");
+          setSecondNum("");
+          setPrevoius("");
+          setMath("");
+        }
+      } else {
+        setEqual("");
+        setOperations("");
+        setSecondNum("");
+        setPrevoius("");
+        setMath("");
+      }
+      if(current<0){
+        setCurrent('Invalid Input')
+      }
+      // setPrevoius(current)
+      let prv = current ? math : current;
+      const result = Math.sqrt(parseFloat(current));
+      setCurrent(result);
+      if (math === "") {
+        setMath("root(" + current + ")");
+      } else {
+        setMath("root(" + prv + ")");
+      }
+    } catch (error) {
+      setCurrent("Invalid Input");
+    }
+  };
+
   const calculateReciprocal = () => {
     try {
-      setPrevoius(current)
-      let val = current ? prevoius : current;
+      if (prevoius && operations) {
+        setSecondNum(math);
+        if (equal) {
+          setEqual("");
+          setOperations("");
+          setSecondNum("");
+          setPrevoius("");
+          setMath("");
+        }
+      } else {
+        setEqual("");
+        setOperations("");
+        setSecondNum("");
+        setPrevoius("");
+        setMath("");
+      }
+
+      let val = current ? math : current;
       const result = (1 / parseFloat(current)).toString();
       setCurrent(result);
-      setPrevoius("1/(" + val + ")");
+      if (math === "") {
+        setMath("1/(" + current + ")");
+      } else {
+        setMath("1/(" + val + ")");
+      }
     } catch (error) {
       setCurrent("ERROR");
     }
@@ -69,14 +132,79 @@ function Calculator() {
 
   const calculateSquare = () => {
     try {
-      setPrevoius(current)
-      let sqr = current ? prevoius : current;
-      const result = (parseFloat(current) * parseFloat(current))
+      if (prevoius && operations) {
+        setSecondNum(math);
+        if (equal) {
+          setEqual("");
+          setOperations("");
+          setSecondNum("");
+          setPrevoius("");
+          setMath("");
+        }
+      } else {
+        setEqual("");
+        setOperations("");
+        setSecondNum("");
+        setPrevoius("");
+        setMath("");
+      }
+      let sqr = current ? math : current;
+      const result = parseFloat(current) * parseFloat(current);
       setCurrent(result);
-      setPrevoius("sqr(" + sqr + ")");
+      if (math === "") {
+        setMath("sqr(" + current + ")");
+      } else {
+        setMath("sqr(" + sqr + ")");
+      }
     } catch (error) {
       setCurrent("ERROR");
     }
+  };
+
+  const signChange = () => {
+    if (current === "") return;
+    if (equal) {
+      setEqual("");
+      setOperations("");
+      setSecondNum("");
+      setPrevoius("");
+      setMath("negate(" + current + ")");
+      setCurrent(-current);
+    }
+    if (math === "") {
+      setMath("negate(" + current + ")");
+    } else {
+      setMath("negate(" + math + ")");
+    }
+    setCurrent(-current);
+    // try {
+    //   if (prevoius && operations) {
+    //     setSecondNum(math);
+    //     if (equal) {
+    //       setEqual("");
+    //       setOperations("");
+    //       setSecondNum("");
+    //       setPrevoius("");
+    //       setMath("");
+    //     }
+    //   } else {
+    //     setEqual("");
+    //     setOperations("");
+    //     setSecondNum("");
+    //     setPrevoius("");
+    //     setMath("");
+    //   }
+    //   let sqr = current ? math : current;
+    //   const result = -current;
+    //   setCurrent(result);
+    //   if (math === "") {
+    //     setMath("sqr(" + current + ")");
+    //   } else {
+    //     setMath("sqr(" + sqr + ")");
+    //   }
+    // } catch (error) {
+    //   setCurrent("ERROR");
+    // }
   };
 
   const compute = () => {
@@ -106,66 +234,89 @@ function Calculator() {
     return result;
   };
 
-
-
   const appendValueHandler = (el) => {
     const value = el;
     console.log("here is the value", value);
-    if (value === "." && current.includes(".")) return;
-    if (current.length < 12) setCurrent(current + value);
+    if (value === ".") {
+      if (current.includes(".")) {
+        return;
+      } else {
+        if (current === "") {
+          setCurrent("0.");
+        }
+      }
+    } else {
+      if (current.length < 12) {
+        setCurrent(current + value);
+      }
+    }
   };
 
   const chooseOperationHandler = (el) => {
     if (current === "") {
-      return;
-    }
-    if (current === ".") {
-      return;
-    }
-    if (prevoius !== "") {
+      setPrevoius("0");
+      setOperations(el);
+    } else if (current === ".") {
+      setPrevoius("0");
+      setOperations(el);
+    } else if (equal) {
+      setEqual("");
+      setSecondNum("");
+      setOperations(el);
       setPrevoius(current);
+    } else if (prevoius) {
+      // setPrevoius(current);
       setOperations(el);
       let value = compute();
       setPrevoius(value);
     } else {
-    setPrevoius(current);
+      setPrevoius(current);
+      setOperations(el);
+    }
     setOperations(el);
-  }
+
     setCurrent("");
   };
 
   const equalHandler = (value) => {
-    if (value === undefined || current === null || value == null) return;
+    if (
+      value === undefined ||
+      current === null ||
+      value == null ||
+      current === undefined
+    )
+      return;
     if (value === "=") {
-      if(current==='') return;
+      if (current === "") return;
+      if (current === "0.") setPrevoius("0");
       let ans = compute();
-      setOperations(operations + current + value);
+      setOperations(operations);
+      prevoius ? setSecondNum(current) : setPrevoius(current);
+      setEqual(value);
       setCurrent(ans);
     }
   };
-
-
 
   let buttons = [
     {
       value: "%",
       opr: true,
-      onclick: () => calculatePercentage()
+      onclick: () => calculatePercentage(),
     },
     {
       value: "CE",
       opr: true,
-      onclick: () => clearCurrent("C")
+      onclick: () => clearCurrent("C"),
     },
     {
       value: "C",
       opr: true,
-      onclick: () => allclearHandler("CE")
+      onclick: () => allclearHandler("CE"),
     },
     {
       value: <BsBackspace />,
       opr: true,
-      onclick: () => deleteHandler("dl")
+      onclick: () => deleteHandler("dl"),
     },
     {
       value: (
@@ -174,7 +325,7 @@ function Calculator() {
         </div>
       ),
       opr: true,
-      onclick: () => calculateReciprocal()
+      onclick: () => calculateReciprocal(),
     },
     {
       value: (
@@ -183,7 +334,7 @@ function Calculator() {
         </div>
       ),
       opr: true,
-      onclick: () => calculateSquare()
+      onclick: () => calculateSquare(),
     },
     {
       value: (
@@ -193,63 +344,63 @@ function Calculator() {
         </div>
       ),
       opr: true,
-      onclick: () => calculateRoot()
+      onclick: () => calculateRoot(),
     },
     {
       value: <PiDivideLight />,
       opr: true,
-      onclick: () => chooseOperationHandler("/")
+      onclick: () => chooseOperationHandler("/"),
     },
     {
       value: "7",
-      onclick: () => appendValueHandler("8")
+      onclick: () => appendValueHandler("8"),
     },
     {
       value: "8",
-      onclick: () => appendValueHandler("7")
+      onclick: () => appendValueHandler("7"),
     },
     {
       value: "9",
-      onclick: () => appendValueHandler("9")
+      onclick: () => appendValueHandler("9"),
     },
     {
       value: <RxCross1 />,
       opr: true,
-      onclick: () => chooseOperationHandler("*")
+      onclick: () => chooseOperationHandler("*"),
     },
     {
       value: "4",
-      onclick: () => appendValueHandler("4")
+      onclick: () => appendValueHandler("4"),
     },
     {
       value: "5",
-      onclick: () => appendValueHandler("5")
+      onclick: () => appendValueHandler("5"),
     },
     {
       value: "6",
-      onclick: () => appendValueHandler("6")
+      onclick: () => appendValueHandler("6"),
     },
     {
       value: <GoDash />,
       opr: true,
-      onclick: () => chooseOperationHandler("-")
+      onclick: () => chooseOperationHandler("-"),
     },
     {
       value: "1",
-      onclick: () => appendValueHandler("1")
+      onclick: () => appendValueHandler("1"),
     },
     {
       value: "2",
-      onclick: () => appendValueHandler("2")
+      onclick: () => appendValueHandler("2"),
     },
     {
       value: "3",
-      onclick: () => appendValueHandler("3")
+      onclick: () => appendValueHandler("3"),
     },
     {
       value: <GoPlus />,
       opr: true,
-      onclick: () => chooseOperationHandler("+")
+      onclick: () => chooseOperationHandler("+"),
     },
     {
       value: (
@@ -257,30 +408,26 @@ function Calculator() {
           <sup>+</sup>/<sub>-</sub>
         </div>
       ),
+      onclick: () => signChange(),
     },
     {
       value: "0",
-      onclick: () => appendValueHandler("0")
+      onclick: () => appendValueHandler("0"),
     },
     {
       value: ".",
-      onclick: () => appendValueHandler(".")
+      onclick: () => appendValueHandler("."),
     },
     {
       value: "=",
       opr: true,
       eq: true,
-      onclick: () => equalHandler("=")
+      onclick: () => equalHandler("="),
     },
   ];
 
   return (
     <div className="main">
-      <div className="backToHome">
-        <Link to='/'>
-        <FcHome style={{width:'50px',height:'50px'}}/>
-        </Link>
-      </div>
       <div className="calculator">
         <div className="display">
           <div
@@ -321,11 +468,14 @@ function Calculator() {
           <div style={{ maxWidth: "330px" }}>
             <div className="h4" style={{ height: "30px" }}>
               {prevoius}
+
               {operations}
+
+              {seconNum ? seconNum : math}
+
+              {equal}
             </div>
-            <div className="h1">
-              {current ? current : '0' }
-            </div>
+            <div className="h1">{current ? current : "0"}</div>
           </div>
         </div>
         <div className="numberpad">
